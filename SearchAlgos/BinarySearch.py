5e1610588f70f888aa4952680658d2e3a0d92fed
Henry Fritz <133249998+hfritz34@users.noreply.github.com>
2025-08-18 13:42:16 -0400

    Binary Search ;)

---
+++ SearchAlgos/BinarySearch.py
@@ -0,0 +1,23 @@
+def binarySearch(arr, target):
+    l = 0
+    r = len(arr) - 1
+
+    while l <= r:
+        mid = (l+r) // 2
+        
+        if target > arr[mid]:
+            l = mid + 1
+            
+        elif target < arr[mid]:
+            r = mid - 1
+
+        else:
+            return mid
+    return -1
+
+
+print(binarySearch([1,2,3,4,5,6,7,8], 8))
+
+
+
+ 
